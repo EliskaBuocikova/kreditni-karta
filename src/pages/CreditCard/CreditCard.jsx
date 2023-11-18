d5cf244025054db8fdaf885afe0e5e9141453ffb
Eliška Buociková <buocikovaeliska@gmail.com>
2023-11-18 15:09:19 +0100

    funkčnost aplikace

--- src/pages/CreditCard/CreditCard.jsx
+++ src/pages/CreditCard/CreditCard.jsx
@@ -1,10 +1,64 @@
+import { useState, useEffect, useRef } from 'react';
+
 const CreditCard = () => {
+  const [creditCard, setCreditCard] = useState({
+    input1: '',
+    input2: '',
+    input3: '',
+    input4: '',
+  });
+
+  const inputRefs = [useRef(), useRef(), useRef(), useRef()];
+
+  const handleChange = (field, value) => {
+    setCreditCard({
+      ...creditCard,
+      [field]: value,
+    });
+  };
+
+  useEffect(() => {
+    const { input1, input2, input3, input4 } = creditCard;
+
+    const inputs = [input1, input2, input3, input4];
+
+    for (let index = 0; index < inputs.length; index++) {
+      if (inputs[index].length === 4 && index < inputs.length - 1) {
+        inputRefs[index + 1].current.focus();
+      }
+    }
+  }, [creditCard]);
+
   return (
     <div className="credit-card">
-      <input />
-      <input />
-      <input />
-      <input />
+      <input
+        ref={inputRefs[0]}
+        type="text"
+        maxLength="4"
+        value={creditCard.input1}
+        onChange={(event) => handleChange('input1', event.target.value)}
+      />
+      <input
+        ref={inputRefs[1]}
+        type="text"
+        maxLength="4"
+        value={creditCard.input2}
+        onChange={(event) => handleChange('input2', event.target.value)}
+      />
+      <input
+        ref={inputRefs[2]}
+        type="text"
+        maxLength="4"
+        value={creditCard.input3}
+        onChange={(event) => handleChange('input3', event.target.value)}
+      />
+      <input
+        ref={inputRefs[3]}
+        type="text"
+        maxLength="4"
+        value={creditCard.input4}
+        onChange={(event) => handleChange('input4', event.target.value)}
+      />
     </div>
   );
 };
