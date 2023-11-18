1d740a1c2627da587f44086be38d41057405d5a4
Eliška Buociková <buocikovaeliska@gmail.com>
2023-11-18 15:31:27 +0100

    úprava stylování

--- src/pages/CreditCard/CreditCard.jsx
+++ src/pages/CreditCard/CreditCard.jsx
@@ -30,36 +30,39 @@ const CreditCard = () => {
   }, [creditCard]);
 
   return (
-    <div className="credit-card">
-      <input
-        ref={inputRefs[0]}
-        type="text"
-        maxLength="4"
-        value={creditCard.input1}
-        onChange={(event) => handleChange('input1', event.target.value)}
-      />
-      <input
-        ref={inputRefs[1]}
-        type="text"
-        maxLength="4"
-        value={creditCard.input2}
-        onChange={(event) => handleChange('input2', event.target.value)}
-      />
-      <input
-        ref={inputRefs[2]}
-        type="text"
-        maxLength="4"
-        value={creditCard.input3}
-        onChange={(event) => handleChange('input3', event.target.value)}
-      />
-      <input
-        ref={inputRefs[3]}
-        type="text"
-        maxLength="4"
-        value={creditCard.input4}
-        onChange={(event) => handleChange('input4', event.target.value)}
-      />
-    </div>
+    <>
+      <h2 className="title">Zadejte číslo kreditní karty:</h2>
+      <div className="credit-card">
+        <input
+          ref={inputRefs[0]}
+          type="text"
+          maxLength="4"
+          value={creditCard.input1}
+          onChange={(event) => handleChange('input1', event.target.value)}
+        />
+        <input
+          ref={inputRefs[1]}
+          type="text"
+          maxLength="4"
+          value={creditCard.input2}
+          onChange={(event) => handleChange('input2', event.target.value)}
+        />
+        <input
+          ref={inputRefs[2]}
+          type="text"
+          maxLength="4"
+          value={creditCard.input3}
+          onChange={(event) => handleChange('input3', event.target.value)}
+        />
+        <input
+          ref={inputRefs[3]}
+          type="text"
+          maxLength="4"
+          value={creditCard.input4}
+          onChange={(event) => handleChange('input4', event.target.value)}
+        />
+      </div>
+    </>
   );
 };
 
